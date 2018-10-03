1b7dd0b6fd4806532ed93738f5abf3e82653d832
Vadym Fedyukovych <vadym@platin.io>
2018-10-03 14:28:09 +0300

    re-enable negative exponents for coordinate commitments

--- lib/commitments.cpp
+++ lib/commitments.cpp
@@ -6,14 +6,39 @@
 #include <iostream>
 #include "proofs.hpp"
 
-//#define DBG_NEGEXP
+//#define NEED_NEGEXP
+#define DBG_NEGEXP
 //#define DBG_LOCCOMM
 //#define DBG_ACOMM
 //#define DBG_NCOMM
 
+#ifdef NEED_NEGEXP
+CryptoPP::Integer neg_a_exp_b_mod_c(const CryptoPP::Integer& a, const CryptoPP::Integer& b, const CryptoPP::ModularArithmetic& c) {
+  CryptoPP::Integer interm, result;
+
+  if(b < 0) {
+    interm = c.Exponentiate(a, -b);
+    result = c.MultiplicativeInverse(interm);
+  } else {
+    result = c.Exponentiate(a, b);
+    interm = c.MultiplicativeInverse(result);
+  }
+  return result;
+}
+#endif
+
 CryptoPP::Integer CreateCommitment(const Parameters &pp, const CryptoPP::Integer x, const CryptoPP::Integer y, const CryptoPP::Integer z, const CryptoPP::Integer r) {
   CryptoPP::Integer s;
 
+#ifdef NEED_NEGEXP
+  s = pp.group.Multiply(
+          neg_a_exp_b_mod_c(pp.gx, x, pp.group),
+          pp.group.Multiply(
+             neg_a_exp_b_mod_c(pp.gy, y, pp.group),
+             pp.group.Multiply(
+                neg_a_exp_b_mod_c(pp.gz, z, pp.gz),
+                neg_a_exp_b_mod_c(pp.gr, r, pp.group))));
+#else // need negative
 #ifdef DBG_NEGEXP
   if(x < 0 || y < 0 || z < 0 || r < 0) {
     std::cout << "Negative exp at CreateCommitment()" << std::endl
@@ -24,7 +49,8 @@ CryptoPP::Integer CreateCommitment(const Parameters &pp, const CryptoPP::Integer
 	      << std::endl;
     return CryptoPP::Integer::Zero();
   }
-#endif
+#endif // debug negative
+#endif // need negative
   s = pp.group.Multiply(
 	  pp.group.Exponentiate(pp.gx, x),
 	  pp.group.Multiply(
